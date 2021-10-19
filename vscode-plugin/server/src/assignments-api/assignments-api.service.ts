0dcd695362cefea28ca217ba09fa221d88d1eaef
Adrian Kunz <clashsoft@hotmail.com>
2021-10-19 16:16:40 +0200

    refactor(vscode-plugin): Simplify AssignmentsApiService

--- vscode-plugin/server/src/assignments-api/assignments-api.service.ts
+++ vscode-plugin/server/src/assignments-api/assignments-api.service.ts
@@ -1,5 +1,6 @@
 import {HttpService} from '@nestjs/axios';
 import {Injectable} from '@nestjs/common';
+import {AxiosRequestConfig} from 'axios';
 import {firstValueFrom} from 'rxjs';
 import {Config} from '../config/config';
 import {Annotation, AnnotationDto} from './annotation';
@@ -24,60 +25,66 @@ export class AssignmentsApiService {
   }
 
   async getAssignment(config: Config): Promise<Assignment> {
-    const {data: assignment} = await firstValueFrom(this.httpService.get(`${config.apiServer}/api/v1/assignments/${config.assignment.id}`, {
-      headers: {
-        'Assignment-Token': config.assignment.token,
-      },
-    }));
-    return assignment;
+    return this.http('GET', `${config.apiServer}/api/v1/assignments/${config.assignment.id}`, undefined, {
+      headers: this.getHeaders(config),
+    });
   }
 
   async getSolution(config: Config, github: string): Promise<Solution> {
-    const {data: solutions} = await firstValueFrom(this.httpService.get(`${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions`, {
+    const all = await this.http<Solution[]>('GET', `${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions`, undefined, {
       params: {
         'author.github': github,
       },
-      headers: {
-        'Assignment-Token': config.assignment.token,
-      },
-    }));
-    return solutions[0];
+      headers: this.getHeaders(config),
+    });
+    return all[0];
   }
 
   async createAnnotation(config: Config, solution: string, dto: AnnotationDto): Promise<Annotation> {
-    const {data} = await firstValueFrom(this.httpService.post<Annotation>(`${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations`, dto, {
-      headers: {
-        'Assignment-Token': config.assignment.token,
-      },
-    }));
-    return data;
+    return this.http<Annotation>('POST', `${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations`, dto, {
+      headers: this.getHeaders(config),
+    });
   }
 
-  async getAnnotations(config: Config, solution: string, params?: {task?: string, file?: string}): Promise<Annotation[]> {
-    const {data} = await firstValueFrom(this.httpService.get<Annotation[]>(`${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations`, {
+  async getAnnotations(config: Config, solution: string, params?: { task?: string, file?: string }): Promise<Annotation[]> {
+    return this.http<Annotation[]>('GET', `${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations`, undefined, {
       params,
-      headers: {
-        'Assignment-Token': config.assignment.token,
-      },
-    }));
-    return data;
+      headers: this.getHeaders(config),
+    });
   }
 
   async getAnnotation(config: Config, solution: string, id: string): Promise<Annotation> {
-    const {data} = await firstValueFrom(this.httpService.get<Annotation>(`${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations/${id}`, {
-      headers: {
-        'Assignment-Token': config.assignment.token,
-      },
-    }));
-    return data;
+    return this.http<Annotation>('GET', `${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations/${id}`, undefined, {
+      headers: this.getHeaders(config),
+    });
   }
 
   async updateAnnotation(config: Config, solution: string, id: string, dto: Partial<AnnotationDto>): Promise<Annotation> {
-    const {data} = await firstValueFrom(this.httpService.patch<Annotation>(`${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations/${id}`, dto, {
-      headers: {
-        'Assignment-Token': config.assignment.token,
-      },
-    }));
-    return data;
+    return this.http<Annotation>('PATCH', `${config.apiServer}/api/v1/assignments/${config.assignment.id}/solutions/${solution}/annotations/${id}`, dto, {
+      headers: this.getHeaders(config),
+    });
+  }
+
+  private async http<T>(method: string, url: string, body?: any, options?: AxiosRequestConfig): Promise<T> {
+    try {
+      const {data} = await firstValueFrom(this.httpService.request({
+        ...options,
+        method,
+        url,
+        data: body,
+      }));
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  private getHeaders(config: Config): any {
+    const result: Record<string, string> = {};
+    if (config.assignment.token) {
+      result['Assignment-Token'] = config.assignment.token;
+    }
+    return result;
   }
 }
