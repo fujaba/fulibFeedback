664f78d3fdfc769b252023b9834ae9042db85cac
Adrian Kunz <clashsoft@hotmail.com>
2021-11-24 13:38:00 +0100

    fix(vscode-plugin): Remove invalid server tests

--- vscode-plugin/server/src/task-matcher/task-matcher.service.spec.ts
+++ vscode-plugin/server/src/task-matcher/task-matcher.service.spec.ts
@@ -14,29 +14,6 @@ describe('TaskMatcherService', () => {
     service = module.get<TaskMatcherService>(TaskMatcherService);
   });
 
-  it('should match Tasks', () => {
-    const dummy: Task = {_id: '', children: [], points: 0, description: ''};
-    const foo: Task = {
-      ...dummy,
-      description: '`foo`',
-    };
-    expect(service.getApplicableRange(foo, text)).toEqual([5, 14]);
-
-    const fooBar: Task = {
-      ...dummy,
-      description: '`foo` `bar`',
-    };
-    expect(service.getApplicableRange(fooBar, text)).toEqual([11, 12]);
-  })
-
-  it('should match brackets', () => {
-    expect(service.findMatchingBrace(text, 4, text.length)).toBe(14);
-    expect(service.findMatchingBrace(text, 10, text.length)).toBe(12);
-    expect(service.findMatchingBrace(text, 4, text.length - 1)).toBe(-1);
-    expect(service.findMatchingBrace(text, 10, text.length - 1)).toBe(12);
-    expect(service.findMatchingBrace(text, 10, text.length - 3)).toBe(-1);
-  })
-
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
