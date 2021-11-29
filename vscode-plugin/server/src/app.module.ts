8db060615dce60e0b3de9375798429889728f9b5
Adrian Kunz <clashsoft@hotmail.com>
2021-11-29 23:40:14 +0100

    refactor(vscode-plugin): Remove TaskMatcherService

--- vscode-plugin/server/src/app.module.ts
+++ vscode-plugin/server/src/app.module.ts
@@ -6,7 +6,6 @@ import {DocumentService} from './document/document.service';
 import {ValidationService} from './validation/validation.service';
 import { ActionService } from './action/action.service';
 import { AssignmentsApiService } from './assignments-api/assignments-api.service';
-import { TaskMatcherService } from './task-matcher/task-matcher.service';
 
 @Module({
   imports: [
@@ -20,7 +19,6 @@ import { TaskMatcherService } from './task-matcher/task-matcher.service';
     ValidationService,
     ActionService,
     AssignmentsApiService,
-    TaskMatcherService,
   ],
 })
 export class AppModule {
